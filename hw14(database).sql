f1559840c22340dd408ef4589a1d8cb7e0448b0f
Yuliya Meremyanina <lastt@mail.ru>
2022-05-19 21:34:29 +0300

    hw14

---
+++ hw14(database).sql
@@ -0,0 +1,16 @@
+﻿CREATE DATABASE IntegrationDB
+ON PRIMARY
+( NAME = IntegrationDB_dat,
+    FILENAME = 'C:\OTUS_MSSQL_Repo\Data\IntegrationDB.mdf',
+    SIZE = 10,
+    MAXSIZE = 50,
+    FILEGROWTH = 5 )
+LOG ON
+( NAME = IntegrationDB_log,
+    FILENAME = 'C:\OTUS_MSSQL_Repo\Log\IntegrationDB.ldf',
+    SIZE = 5MB,
+    MAXSIZE = 25MB,
+    FILEGROWTH = 5MB )
+COLLATE cyrillic_general_ci_as;
+
+ALTER DATABASE IntegrationDB SET RECOVERY SIMPLE;
---
+++ hw14(tables).sql
@@ -0,0 +1,48 @@
+﻿USE IntegrationDB
+
+CREATE TABLE dbo.Tasks
+(Id bigint identity primary key clustered,
+SourceSystemId uniqueidentifier,
+TargetSystemId bigint null, 
+IntagrationDate datetime2,
+CONSTRAINT UNQ_Tasks_SourceSystemId UNIQUE (SourceSystemId))
+
+CREATE TABLE dbo.Files
+(Id bigint identity primary key clustered,
+SourceSystemId uniqueidentifier,
+TargetSystemId bigint null, 
+TaskId bigint REFERENCES dbo.Tasks(Id),
+FileString varchar(max),
+FileName varchar(250),
+IntagrationDate datetime2,
+CONSTRAINT UNQ_Files_SourceSystemId UNIQUE (SourceSystemId))
+
+CREATE TABLE dbo.Errors
+(Id bigint identity primary key clustered,
+TaskId bigint REFERENCES dbo.Tasks(Id),
+ErrorMessage varchar(500),
+ErrorDate datetime2)
+
+CREATE TABLE dbo.Quenue
+(Id bigint identity primary key clustered,
+SourceSystemId uniqueidentifier,
+CONSTRAINT UNQ_Quenue_SourceSystemId UNIQUE (SourceSystemId))
+
+CREATE INDEX IX_SourceSystemId ON dbo.Tasks
+(SourceSystemId)
+INCLUDE (TargetSystemId)
+
+CREATE INDEX IX_SourceSystemId ON dbo.Files
+(SourceSystemId)
+INCLUDE (TargetSystemId)
+
+CREATE INDEX IX_ErrorDate ON dbo.Errors
+(ErrorDate)
+
+CREATE INDEX IX_SourceSystemId ON dbo.Quenue
+(SourceSystemId)
+
+--добавила для ДЗ, но в проект я добавлять такое не стану, так как критична производительность, а эта проверка будет на стороне API
+ALTER TABLE dbo.Files
+   ADD CONSTRAINT CHK_Files_FileName   
+   CHECK (FileName like '%.%')
